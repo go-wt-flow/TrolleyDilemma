afd56c38766faf957dce5acaaf5fc7457fc4084f
Kim-kiseong <111440691+Kim-kiseong@users.noreply.github.com>
2026-02-16 01:24:16 +0900

    Add steering_motor

---
+++ src/RCCar/_13_steering_motor.py
@@ -0,0 +1,32 @@
+from _12_servo_motor import ServoMotor
+
+class SteeringMotor(ServoMotor):
+    def __init__(self, pwm):
+        super().__init__(pwm)
+        
+    
+    def turnLeft(self):
+        self.setAngle(0)
+    
+    def turnForward(self):
+        self.setAngle(int(90*1.1))
+
+    
+    def turnRight(self):
+        self.setAngle(180)
+    
+    
+if __name__=='__main__':
+    steeringMotor=SteeringMotor(7)
+        
+    
+    while True:
+        cmd = input('left:l, middle:m, right:r >> ')
+        if cmd =='l':
+            steeringMotor.turnLeft()
+            
+        elif cmd =='m':
+            steeringMotor.turnForward()
+            
+        elif cmd == 'r':
+            steeringMotor.turnRight()
